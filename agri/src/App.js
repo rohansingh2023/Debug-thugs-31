934567de9d179ca7e10137db1e119fc5d91ed8c1
Tejas <tejaspatne18@gmail.com>
2023-03-02 22:41:06 +0530

    created seller's form

--- agri/src/App.js
+++ agri/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import Seller from './pages/Seller';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
+          <Route path='/sell' element={<Seller/>}></Route>
         </Routes>
       </BrowserRouter>
       <Footer/>
