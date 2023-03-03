ee0375f49819e93e3eb44d841d8bc11b07faab0f
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2023-03-03 13:34:58 +0530

    bg blur and crops data added

---
+++ agri/src/data/crops.js
@@ -0,0 +1,60 @@
+const data = [
+  {
+    title: "Wheat",
+    quantity: 2,
+    img: "https://tse1.mm.bing.net/th?id=OIP.zfkVnZ7kRfddKFfTex5I5wHaFl&pid=Api&rs=1&c=1&qlt=95&w=147&h=110",
+    location:
+      "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
+  },
+  {
+    title: "Rice",
+    quantity: 2,
+    img: "https://th.bing.com/th/id/OIP.HxW0vkNneyf-6PNOWLQPmgHaE8?w=235&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    location:
+      "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
+  },
+  {
+    title: "Jowar",
+    quantity: 2,
+    img: "https://th.bing.com/th/id/OIP.7uxvR11_FHgbW0lwuTC-jAHaFE?w=231&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    location:
+      "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
+  },
+  {
+    title: "Ragi",
+    quantity: 2,
+    img: "https://th.bing.com/th/id/OIP.XNN0lVDfuIAEcqHqhJ4V8wHaHa?w=180&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    location:
+      "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
+  },
+  {
+    title: "Maize",
+    quantity: 2,
+    img: "https://th.bing.com/th/id/OIP.uV02m2V9WAWkKlZnVB4NpgHaEo?w=252&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    location:
+      "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
+  },
+  {
+    title: "Urad Daal",
+    quantity: 2,
+    img: "https://th.bing.com/th/id/OIP.f-pBnIpRvJ-4whRMn0aDbgHaHa?w=198&h=198&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    location:
+      "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
+  },
+  {
+    title: "Bajra",
+    quantity: 2,
+    img: "https://th.bing.com/th/id/OIP._smpfPkkM7m4rzM6vrX65gHaE6?w=280&h=186&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    location:
+      "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
+  },
+  {
+    title: "Tomato",
+    quantity: 2,
+    img: "https://th.bing.com/th/id/OIP.j5O7NEjgy8-h_lvatF4pAQHaFj?w=255&h=192&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    location:
+      "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
+  },
+];
+
+export default data;
--- agri/src/pages/Buy.jsx
+++ agri/src/pages/Buy.jsx
@@ -1,110 +1,53 @@
 import React from "react";
-// import "./Buy.css";
 import BuyCard from "../components/BuyCard/Buycard";
+import data from "../data/crops";
 
 const Buy = () => {
-  const data = [
-    {
-      title: "Wheat",
-      quantity: 2,
-      img: "https://tse1.mm.bing.net/th?id=OIP.zfkVnZ7kRfddKFfTex5I5wHaFl&pid=Api&rs=1&c=1&qlt=95&w=147&h=110",
-      location:
-        "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
-    },
-    {
-      title: "Wheat",
-      quantity: 2,
-      img: "https://tse1.mm.bing.net/th?id=OIP.zfkVnZ7kRfddKFfTex5I5wHaFl&pid=Api&rs=1&c=1&qlt=95&w=147&h=110",
-      location:
-        "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
-    },
-    {
-      title: "Wheat",
-      quantity: 2,
-      img: "https://tse1.mm.bing.net/th?id=OIP.zfkVnZ7kRfddKFfTex5I5wHaFl&pid=Api&rs=1&c=1&qlt=95&w=147&h=110",
-      location:
-        "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
-    },
-    {
-      title: "Wheat",
-      quantity: 2,
-      img: "https://tse1.mm.bing.net/th?id=OIP.zfkVnZ7kRfddKFfTex5I5wHaFl&pid=Api&rs=1&c=1&qlt=95&w=147&h=110",
-      location:
-        "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
-    },
-    {
-      title: "Wheat",
-      quantity: 2,
-      img: "https://tse1.mm.bing.net/th?id=OIP.zfkVnZ7kRfddKFfTex5I5wHaFl&pid=Api&rs=1&c=1&qlt=95&w=147&h=110",
-      location:
-        "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
-    },
-    {
-      title: "Wheat",
-      quantity: 2,
-      img: "https://tse1.mm.bing.net/th?id=OIP.zfkVnZ7kRfddKFfTex5I5wHaFl&pid=Api&rs=1&c=1&qlt=95&w=147&h=110",
-      location:
-        "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
-    },
-    {
-      title: "Wheat",
-      quantity: 2,
-      img: "https://tse1.mm.bing.net/th?id=OIP.zfkVnZ7kRfddKFfTex5I5wHaFl&pid=Api&rs=1&c=1&qlt=95&w=147&h=110",
-      location:
-        "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
-    },
-    {
-      title: "Wheat",
-      quantity: 2,
-      img: "https://tse1.mm.bing.net/th?id=OIP.zfkVnZ7kRfddKFfTex5I5wHaFl&pid=Api&rs=1&c=1&qlt=95&w=147&h=110",
-      location:
-        "58MG+MX6, South Central Railway Colony, Nanded, Maharashtra 431605",
-    },
-  ];
-
   return (
-    // <div className="flex before:bg-test before:bg-no-repeat before:bg-cover before:bg-center before:bg-fixed before:opacity-70 overflow-hidden">
-    <div className="flex bg-buy bg-no-repeat bg-cover bg-center bg-fixed">
-      <div className="flex-[0.25]  sticky max-h-screen w-11/12 mt-[5vh] overflow-hidden">
-        <div className="bg-slate-200 py-5 px-5 space-y-10 ml-[3vh] mt-[3vh] rounded-md shadow-md">
-          <h1 className="text-3xl font-bold">Search For Products</h1>
-          <p className="text-xl font-semibold">Filter by:</p>
-          <div className="flex items-center space-x-5">
-            {/* <p>Title</p> */}
-            <input
-              type="text"
-              placeholder="Crop Name"
-              className="rounded-lg p-1 w-full"
-            />
-          </div>
-          <div className="flex items-center space-x-5">
-            {/* <p>Title</p> */}
-            <input
-              type="text"
-              placeholder="Quantity"
-              className="rounded-lg p-1 w-full"
-            />
-          </div>
-          <div className="flex items-center space-x-5">
-            {/* <p>Title</p> */}
-            <input
-              type="text"
-              placeholder="Location"
-              className="rounded-lg p-1 w-full"
-            />
-          </div>
+    <div className="relative h-screen w-screen">
+      <div className="absolute top-0 left-0 opacity-30 bg-buy bg-no-repeat bg-cover bg-center bg-fixed h-full w-full"></div>
+      <div className="flex absolute top-0 left-0 h-full w-full">
+        <div className="flex-[0.25]  sticky max-h-screen w-11/12 mt-[5vh] overflow-hidden">
+          <div className="bg-slate-200 py-5 px-5 space-y-10 ml-[3vh] mt-[3vh] rounded-md shadow-lg shadow-slate-700">
+            <h1 className="text-3xl font-bold">Search For Products</h1>
+            <p className="text-xl font-semibold">Filter by:</p>
+            <div className="flex items-center space-x-5">
+              {/* <p>Title</p> */}
+              <input
+                type="text"
+                placeholder="Crop Name"
+                className="rounded-lg p-1 w-full"
+              />
+            </div>
+            <div className="flex items-center space-x-5">
+              {/* <p>Title</p> */}
+              <input
+                type="text"
+                placeholder="Quantity"
+                className="rounded-lg p-1 w-full"
+              />
+            </div>
+            <div className="flex items-center space-x-5">
+              {/* <p>Title</p> */}
+              <input
+                type="text"
+                placeholder="Location"
+                className="rounded-lg p-1 w-full"
+              />
+            </div>
 
-          <button className="p-2 bg-green-400 rounded-md w-full hover:bg-green-600">
-            Search
-          </button>
-        </div>
-      </div>
-      <div className="flex-[0.75] flex items-center max-h-screen overflow-y-scroll justify-start  flex-wrap ml-[10vh]">
-        {data.map((d, i) => (
-          <div className="h-[420px] w-[300px] mr-[80px] mt-[50px]">
-            <BuyCard d={d} key={i} />
+            <button className="p-2 bg-green-400 rounded-md w-full hover:bg-green-600">
+              Search
+            </button>
           </div>
-        ))}
+        </div>
+        <div className="flex-[0.75] flex items-center max-h-screen overflow-y-scroll justify-start  flex-wrap ml-[10vh]">
+          {data.map((d, i) => (
+            <div className="h-[420px] w-[300px] mr-[60px] mt-[50px]">
+              <BuyCard d={d} key={i} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
