2225c1976995cf7c944c96accc6aa984a9766e37
Priyanshu <priyanshusingh153045@gmail.com>
2023-03-03 08:35:29 +0530

    storage

--- agri/src/App.js
+++ agri/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import Storage from './pages/Storage';
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
+        </Routes>
+        <Routes>
+          <Route path='/Storage' element={<Storage/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
