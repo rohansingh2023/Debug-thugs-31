beec198dcf5f14cb8fc3bb19780e05908285cd83
Priyanshu <priyanshusingh153045@gmail.com>
2023-03-04 00:47:40 +0530

    added

--- agri/src/components/Navbar.jsx
+++ agri/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <nav class=" border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 bg-gray-300">
+      <nav class=" border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 bg-gray-900">
         <div class="container flex flex-wrap items-center justify-between mx-auto">
           <a href="" class="flex items-center">
             <img
@@ -11,7 +11,7 @@ const Navbar = () => {
               class="h-6 mr-3 sm:h-9"
               alt="Flowbite Logo"
             />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            <span class="self-center text-xl font-semibold whitespace-nowrap text-white">
               AgroTrade
             </span>
           </a>
@@ -38,12 +38,12 @@ const Navbar = () => {
             </svg>
           </button>
           <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 bg-gray-300">
+            <ul class="flex flex-col p-4 mt-4 bg-gray-900 border  rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark">
+              
               <li>
                 <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-white bg-white-700 rounded md:bg-transparent md:text-white-700 md:p-0 dark:text-white text-xl"
-                  aria-current="page"
+                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-white-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl"
                 >
                   Home
                 </a>
--- agri/src/pages/Home.jsx
+++ agri/src/pages/Home.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 
 const Home = () => {
-    return (
-        
-
- 
+  return (
     <div class="relative">
-        <img class="w-full" src="../images/plant1.jpg" />
-        <h1 class="absolute p-7 text-5xl text-white top-1/2 left-1/5">
-            We BRIDGE Gaps</h1>
-        
+      <img class="w-full" src="../images/plant1.jpg" />
+      <div class="absolute top-1/3 left-1/5 w-5/12 ml-7">
+        <h1 class=" text-5xl text-white ">Farming is a profession of hope.</h1>
+        <p class="text-xl text-white mt-4 mb-4">
+          AgroTrade is a e commerce website where farmers can sell their
+          products to consumers
+        </p>
+        <div>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-5">
+            Explore
+          </button>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            AgroTrade
+          </button>
+        </div>
+      </div>
     </div>
-        
-    )
-}
+  );
+};
 
-
-export default Home
-
+export default Home;
