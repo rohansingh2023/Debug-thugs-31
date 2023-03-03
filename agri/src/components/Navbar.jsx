8001fd8e999dce51752184499cb32839d1ec62f1
Sneha150 <2020.sneha.kadambala@ves.ac.in>
2023-03-03 22:21:12 +0530

    modified home navbar and footer

--- agri/src/components/Navbar.jsx
+++ agri/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-white bg-white-700 rounded md:bg-transparent md:text-white-700 md:p-0 dark:text-white text-xl"
+                  class="block py-2 pl-3 pr-4 text-white bg-gray-700 rounded md:bg-transparent md:text-white-700 md:p-0 dark:text-white text-xl"
                   aria-current="page"
                 >
                   Home
@@ -51,7 +51,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-white-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl"
+                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl"
                 >
                   About
                 </a>
@@ -59,7 +59,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-white-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl"
+                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl"
                 >
                   Services
                 </a>
--- agri/src/pages/Home.jsx
+++ agri/src/pages/Home.jsx
@@ -2,38 +2,90 @@ import React from 'react'
 
 const Home = () => {
     return (
-        
-
- 
-    <div class="relative">
-        <img class="w-full" src="../images/plant1.jpg" />
-        <h1 class="absolute p-7 text-5xl text-white top-1/2 left-1/5">
-            We BRIDGE Gaps</h1>
-        
-    </div>
-        
-        // -translate-x-1/2 -translate-y-1/2
-
-
-            /* <img class="w-full h-1/2" src="../images/plant1.jpg" alt="Plants" /> */
-            /* <div class="md:flex" > */
-            /* <div class="">
-                <div class="padding-top:15px">
-                    <button class="h-8 align-center justify-center items-center mx-auto content-center bg-transparent hover:bg-green-500 text-green-700 font-bold hover:text-white py-1 px-4 border border-green-500 hover:border-transparent rounded">
-                        Buy
-                    </button>
-                    <button class="h-8 align-center bg-transparent hover:bg-green-500 text-green-700 font-bold hover:text-white py-1 px-4 border border-green-500 hover:border-transparent rounded">
-                        Sell
-                    </button>
+        <div>
+            <div >
+                <div class="relative">
+                    <img class="w-full" src="../images/plant1.jpg" />
+                    <div class="absolute top-1/3 left-1/5 w-5/12 ml-7">
+                        <h1 class=" text-5xl text-white ">Farming is a profession of hope.</h1>
+                        <p class="text-xl text-white mt-4 mb-4">
+                            AgroTrade is an e commerce website where farmers can sell their
+                            products directly to consumers with minimum transportation cost and no middle man involved.
+                        </p>
+                        <div>
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-5">
+                                Explore
+                            </button>
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                AgroTrade
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div class="grid gap-4 grid-cols-3 flex justify-right mx-auto items-right p-4 object-right" >
-                    <img src="../images/farmer1.jpg" class="rounded-full w-62 h-65 md:w-48 md:h-48 xl:w-64 xl:h-64" />
-                </div> */
+            </div>
+
+            <div class="p-10 grid rounded-1/3 grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10">
+                {/* <!--Card 1--> */}
+                <div class="rounded overflow-hidden shadow-lg hover:scale-105">
+                    <img class="w-full h-1/2" src="../images/vegetable1.jpg" alt="Forest" />
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Fresh Vegetables and Fruits</div>
+                        <p class="text-gray-700 text-base">
+                            Freshly delivered Fruits and Vegetables with cost effectiveness.
+                        </p>
+                        <button class="absolute bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                            Buy
+                        </button>
+                    </div>
+                </div>
+
+                {/* <!--Card 2--> */}
+                <div class="rounded overflow-hidden shadow-lg hover:scale-105">
+                    <img class="w-full h-1/2" src="../images/vehicle1.jpg" alt="Forest" />
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Affordable Transportation</div>
+                        <p class="text-gray-700 text-base">
+                            Farmers can share the transportation cost or by choosing the route in which trucks or goods vehicles are running to the main market.
+                        </p>
+                        <button class="absolute bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                            Ship
+                        </button>
+                    </div>
+                </div>
+
+                {/* <!--Card 3--> */}
+                <div class="rounded overflow-hidden shadow-lg hover:scale-105">
+                    <img class="w-full h-1/2" src="../images/storage2.jpg" alt="Forest" />
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Cold Storage</div>
+                        <p class="text-gray-700 text-base">
+                            The cold storages improving the life and retaining freshness of the products.
+                        </p>
+                        <button class="absolute bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                            Store
+                        </button>
+                    </div>
+                </div>
+                {/* card 4 */}
+                <div class="rounded overflow-hidden shadow-lg hover:scale-105">
+                    <img class="w-full h-1/2" src="../images/farmer1.jpg" alt="Forest" />
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">Happy Farmers</div>
+                        <p class="text-gray-700 text-base">
+                            Produced being reached at time with maximum benefit to the farmers.
+                        </p>
+                        <button class="absolute bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                            Sell
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
 
 
-            /* </div> */
-        
     )
-}
+};
 
 export default Home
